a0edd238ad87165913a557635c8762b748d873d2
11travis21 <ncsutravis@hotmail.com>
2015-10-03 13:58:46 -0700

    Initial setup
    Checked in test client and stubbed out points.java

--- src/TestClient.java
+++ src/TestClient.java
@@ -1,6 +1,37 @@
-/**
- * Created by Snuffles on 10/3/2015.
- */
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+
 public class TestClient
 {
+    public static void main(String[] args) {
+
+        // read the N points from a file
+        In in = new In(args[0]);
+        int N = in.readInt();
+        Point[] points = new Point[N];
+        for (int i = 0; i < N; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.show(0);
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        /*
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        */
+    }
+
 }
